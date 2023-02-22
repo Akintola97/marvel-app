61aa9f1f742dc8954758c93a393c56daf783e871
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-02-21 23:26:16 -0600

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path = '/' element = {<Home />} />
+        <Route path = '/marvel' element = {<Home />} />
       </Routes>
       </BrowserRouter>
     </div>
